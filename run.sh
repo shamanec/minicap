5e10e37032225c930f158fcdcff19a2896da46e2
Nikola Shabanov <60219580+shamanec@users.noreply.github.com>
2022-02-13 13:06:40 +0200

    change path of ndk for docker contaienr

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -exo pipefail
 
 # Build project
 experimental/gradlew -p experimental assembleDebug
-ndk-build NDK_DEBUG=1 1>&2
+../android-ndk/android-ndk-r23b/ndk-build NDK_DEBUG=1 1>&2
 
 # Figure out which ABI and SDK the device has
 abi=$(adb shell getprop ro.product.cpu.abi | tr -d '\r')
